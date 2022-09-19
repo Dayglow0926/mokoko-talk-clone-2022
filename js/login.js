7c8ad839e69af3000afdd7dcf8f3feb73a1757bd
dayglow0926 <dayglow0926@gmail.com>
2022-09-19 16:26:11 +0900

    refactor: 파일명 변경

--- js/login.js
+++ js/login.js
@@ -1,12 +1,17 @@
 const login_form = document.querySelector("#login-form");
+const login_name = document.querySelector("#login-form input");
 const mokoko = document.querySelector("#mokoko");
 const muse = document.querySelector("#muse");
+
+console.log(login_name.value);
 
 function login(event) {
   event.preventDefault();
 
   mokoko.classList.add("animation-none");
   muse.classList.add("animation-add");
+
+  localStorage.setItem("mokoko-name", login_name.value);
 
   setTimeout(submit, 1800);
 }
